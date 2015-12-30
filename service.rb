beca77cd3cf716a64b92e0ff1beda3ce23f8c417
Dmitriy <datonkushin@gmail.com>
2015-12-30 22:57:45 +0500

    Simple web site with sinatra and skeleton framework

---
+++ service.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+require 'sinatra'
+
+get '/ru' do
+  erb :index_ru
+end
+
+get '/en' do
+  erb :index
+end
+
+get '/' do
+  erb :index
+end
+
+
+get '/lessions' do
+  erb :lessions
+end
