ede353cdca1a1950c2ff44fbb7c2065db5090249
Dmitriy <datonkushin@gmail.com>
2016-01-02 14:58:49 +0500

    Added pages for Russian language

--- service.rb
+++ service.rb
@@ -18,6 +18,14 @@ get '/lessions' do
   erb :lessions
 end
 
+get '/ru/lessions' do
+  erb :ru_lessions
+end
+
 get '/improve' do
   erb :improve
+end
+
+get '/ru/improve' do
+  erb :ru_improve
 end
